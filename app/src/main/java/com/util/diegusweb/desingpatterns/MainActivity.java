f87dee0c14b85db45371060455acad1937a53ca5
Diego Rueda <diego.rueda2@gmail.com>
2018-07-16 20:23:37 -0400

    add text in lalayout

--- app/src/main/java/com/util/diegusweb/desingpatterns/MainActivity.java
+++ app/src/main/java/com/util/diegusweb/desingpatterns/MainActivity.java
@@ -2,6 +2,7 @@ package com.util.diegusweb.desingpatterns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text_view);
+
+        BreadFactory breadFactory = new BreadFactory();
+        Bread bread = breadFactory.getBread("BAG");
+
+        textView.setText(new StringBuilder()
+                .append(bread.name())
+                .append(bread.calories())
+                .toString());
     }
 }
