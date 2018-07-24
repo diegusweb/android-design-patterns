ae79a10224394882b3a3a9fd018e52d1ff514661
Diego Rueda <diego.rueda2@gmail.com>
2018-07-23 20:00:47 -0400

    Personalizar barra app toolbars

--- app/src/main/java/com/util/diegusweb/desingpatterns/MainActivity.java
+++ app/src/main/java/com/util/diegusweb/desingpatterns/MainActivity.java
@@ -2,7 +2,11 @@ package com.util.diegusweb.desingpatterns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if(toolbar != null){
+            setSupportActionBar(toolbar);
+
+        }
 
         SandwichBuilder builder = new SandwichBuilder();
 
@@ -38,5 +48,14 @@ public class MainActivity extends AppCompatActivity {
         s3.getIngredients();
         s3.getCalories();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 }
